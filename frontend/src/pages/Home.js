9ba95647bfb1630bfa33ca6c9467cb20673e1588
Rudransh Singh Rajpurohit <rudranshsinghrajpurohit@gmail.com>
2023-12-13 23:04:26 +0530

    API Fix 2

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -7,37 +7,37 @@ import WorkoutDetails from "../components/workoutDetails";
 import WorkoutForm from "../components/WorkoutForm";
 
 const Home = () => {
-  const { workouts, dispatch } = useWorkoutsContext();
-  const { user } = useAuthContext();
+    const { workouts, dispatch } = useWorkoutsContext();
+    const { user } = useAuthContext();
 
-  useEffect(() => {
-    const fetchWorkouts = async () => {
-      const response = await fetch("/api/workouts", {
-        headers: { Authorization: `Bearer ${user.token}` },
-      });
-      const json = await response.json();
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const response = await fetch("https://workout-buddy-liart.vercel.app/api/workouts", {
+                headers: { Authorization: `Bearer ${user.token}` },
+            });
+            const json = await response.json();
 
-      if (response.ok) {
-        dispatch({ type: "SET_WORKOUTS", payload: json });
-      }
-    };
+            if (response.ok) {
+                dispatch({ type: "SET_WORKOUTS", payload: json });
+            }
+        };
 
-    if (user) {
-      fetchWorkouts();
-    }
-  }, [dispatch, user]);
+        if (user) {
+            fetchWorkouts();
+        }
+    }, [dispatch, user]);
 
-  return (
-    <div className="home">
-      <div className="workouts">
-        {workouts &&
-          workouts.map((workout) => (
-            <WorkoutDetails key={workout._id} workout={workout} />
-          ))}
-      </div>
-      <WorkoutForm />
-    </div>
-  );
+    return (
+        <div className="home">
+            <div className="workouts">
+                {workouts &&
+                    workouts.map((workout) => (
+                        <WorkoutDetails key={workout._id} workout={workout} />
+                    ))}
+            </div>
+            <WorkoutForm />
+        </div>
+    );
 };
 
 export default Home;
